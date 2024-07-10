d7b238c360a67c5ed4e895e2b9c3d1b794327153
Agbor Kima <agborkima1@gmail.com>
2024-07-10 13:07:49 -0500

    added celcius checker

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const city = document.getElementById('city')
 const errorSpan = document.getElementById('error-span')
 const wrapper = document.getElementById('wrapper')
 const searchBtn = document.getElementById('search-btn')
+const celciusCheck = document.getElementById('celcius')
 
 
 let weatherObj = {}
 
-async function getWeather(inputValue) {
+async function getWeather(inputValue, celciusBoolean) {
     errorSpan.textContent = "";
     try{
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${inputValue}`)
@@ -21,7 +22,7 @@ async function getWeather(inputValue) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
         weatherObj = await response.json()
-        showWeather()
+        showWeather(celciusBoolean)
         console.log(weatherObj)
     } catch (error) {
         errorSpan.textContent = "Please enter a city";
@@ -30,7 +31,7 @@ async function getWeather(inputValue) {
     
 }
 
-async function showWeather(){
+async function showWeather(celciusBoolean){
     wrapper.innerHTML = ''
 
     const card = document.createElement('div')
@@ -38,7 +39,11 @@ async function showWeather(){
     const location = document.createElement('h1')
     location.textContent = `${weatherObj.location.name}, ${weatherObj.location.region}`
     const temp = document.createElement('h3')
-    temp.textContent = `${weatherObj.current.temp_f} degrees`
+    if(!celciusBoolean){
+        temp.innerHTML = `${weatherObj.current.temp_f}&#176; F`;
+    }else{
+        temp.innerHTML = `${weatherObj.current.temp_c}&#176; C`;
+    }
 
     weatherObj.current.temp_f > 68 ? card.classList.add('warm') : card.classList.add('cold')
 
@@ -56,6 +61,7 @@ form.addEventListener('submit', (e) =>{
     e.preventDefault()
     
     const inputValue = city.value
-    getWeather(inputValue) 
+    const celciusBool = celciusCheck.checked
+    getWeather(inputValue, celciusBool) 
      
 })
